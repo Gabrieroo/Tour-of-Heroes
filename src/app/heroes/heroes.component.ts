96621f560e27bf918c89da857b4bab4dcc70c575
Gabrieroo <gab.bernardes@hotmail.com>
2020-12-07 11:48:46 -0300

    Adicionando servico de mensagem e rotas, e tela de herois

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero'
-import { Heroes } from '../mock-heroes'
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
+
 
 @Component({
   selector: 'app-heroes',
@@ -9,15 +11,21 @@ import { Heroes } from '../mock-heroes'
 })
 export class HeroesComponent implements OnInit {
   
-  heroes = Heroes;
+  heroes: Hero[];
   selectedHero: Hero = undefined
 
-  constructor() { }
+  constructor(private heroService: HeroService, private messageService : MessageService) { }
 
   ngOnInit(): void {
+    this.getHeroes()
   }
   onSelect(hero : Hero): void {
+    this.messageService.clear()
     this.selectedHero = hero
+    this.messageService.add( `HeroesComponent: Selected hero id=${hero.id}`)
+  }
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
     
 }
